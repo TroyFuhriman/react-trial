6cd119afd592362596803b2755a92c494acde602
Troy Fuhriman <TkFuhriman@gmail.com>
2020-08-24 11:13:08 -0600

    minor changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import App1 from "./App1";
 
-test('renders learn react link', () => {
+test(() => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const {} = render(<App1 />);
 });
